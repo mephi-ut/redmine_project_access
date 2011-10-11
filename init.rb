6cabbf84b8b779a3363e8cc6bd395d3fd1a4aa8a
Igor Zubkov <zubkov@nexstep.ua>
2011-10-11 17:50:44 +0300

    Bump version

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_project_access do
   name 'Redmine Cansee plugin'
   author 'Igor Zubkov'
   description 'This is a plugin for Redmine adds hidden users for project'
-  version '0.0.2'
+  version '0.0.3'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 
