a4c4e4844ccdaeb726c0df47223b07e5946ab3d1
Igor Zubkov <zubkov@nexstep.ua>
2011-10-11 18:18:31 +0300

    Don't show Project Access tab in project setting if project is public or project module don't enabled for this project

--- lib/projects_helper_patch.rb
+++ lib/projects_helper_patch.rb
@@ -6,14 +6,13 @@ module ProjectsHelperPatch
       alias_method :_project_settings_tabs, :project_settings_tabs unless method_defined? :_project_settings_tabs
 
       def project_settings_tabs
-        puts 'hi'
         tabs = _project_settings_tabs
-        #tabs << { :name => 'rates', :partial => 'users/rates', :label => :rate_label_rate_history}
-
-        action = {:name => 'project_access', :controller => 'project_access', :action => :index, :partial => 'project_access/settings', :label => :label_project_access}
-
-        tabs << action #if User.current.allowed_to?(action, @project)
-
+        action = { :name => 'project_access',
+                   :controller => 'project_access',
+                   :action => :index,
+                   :partial => 'project_access/settings',
+                   :label => :label_project_access }
+        tabs << action if !@project.is_public && @project.module_enabled?('redmine_project_access')
         return tabs
       end
 
