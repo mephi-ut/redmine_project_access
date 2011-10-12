661c932b3f175225d3a3b3cbb1715615ff349b07
Igor Zubkov <zubkov@nexstep.ua>
2011-10-12 17:52:42 +0300

    Fix permissions

--- lib/user_patch.rb
+++ lib/user_patch.rb
@@ -6,10 +6,23 @@ module UserPatch
       alias_method :rpa_allowed_to?, :allowed_to? unless method_defined? :rpa_allowed_to?
 
       def allowed_to?(action, context, options={}, &block)
-        rpa_allowed_to?(action, context, options, &block) ||
-          (context &&
-          context.is_a?(Project) &&
-          context.check_for_non_member(User.current))
+        result = rpa_allowed_to?(action, context, options, &block)
+        if !result && context && context.is_a?(Project)
+          # No action allowed on archived projects
+          return false unless context.active?
+          # No action allowed on disabled modules
+          return false unless context.allows_to?(action)
+          # Admin users are authorized for anything else
+          return true if admin?
+
+          roles = roles_for_project(context)
+          return false unless roles
+          roles.detect {|role|
+            (context.is_public? || role.member? || context.check_for_non_member(User.current)) &&
+            role.allowed_to?(action) &&
+            (block_given? ? yield(role, self) : true)
+          }
+        end
       end
     end
   end
