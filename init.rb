9887be76ce76275efed0863eabb565f1db881ed2
Igor Zubkov <zubkov@nexstep.ua>
2011-10-26 16:22:43 +0300

    Bump version

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_project_access do
   name 'Redmine Project Access plugin'
   author 'Igor Zubkov'
   description 'This is a plugin for Redmine adds hidden users for project'
-  version '0.0.8'
+  version '0.0.9'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 
