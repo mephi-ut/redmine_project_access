add26304db7522b67e86fb5d4450b07dee8865e1
Anton Tsapov <tsapov@nexstep.ua>
2012-07-16 11:06:48 +0200

    Fix overlapping Redmine method 'allowed_to?'

--- lib/project_access_patches/user_patch.rb
+++ lib/project_access_patches/user_patch.rb
@@ -4,43 +4,26 @@ module ProjectAccessPatches
 module UserPatch
   def self.included(base)
     base.class_eval do
-      def allowed_to?(action, context, options={}, &block)
-        if context && context.is_a?(Project)
-          # No action allowed on archived projects
-          return false unless context.active?
-          # No action allowed on disabled modules
-          return false unless context.allows_to?(action)
-          # Admin users are authorized for anything else
-          return true if admin?
 
+      alias_method :allowed_to_without_project_access?,
+                   :allowed_to? unless method_defined? :allowed_to_without_project_access?
+
+      def allowed_to?(action, context, options={}, &block)
+        if context && context.is_a?(Project) &&
+           context.active? &&
+           context.allows_to?(action)
+          return true if admin?
           roles = roles_for_project(context)
           return false unless roles
-          roles.detect {|role|
-            (context.is_public? || role.member? || context.check_for_non_member(User.current)) &&
-            role.allowed_to?(action) &&
-            (block_given? ? yield(role, self) : true)
-          }
-        elsif context && context.is_a?(Array)
-          # Authorize if user is authorized on every element of the array
-          context.map do |project|
-            allowed_to?(action, project, options, &block)
-          end.inject do |memo,allowed|
-            memo && allowed
+          if roles.detect {|role|
+               context.check_for_non_member(User.current) &&
+               role.allowed_to?(action) &&
+               (block_given? ? yield(role, self) : true)
+             }
+            return true
           end
-        elsif options[:global]
-          # Admin users are always authorized
-          return true if admin?
-
-          # authorize if user has at least one role that has this permission
-          roles = memberships.collect {|m| m.roles}.flatten.uniq
-          roles << (self.logged? ? Role.non_member : Role.anonymous)
-          roles.detect {|role|
-            role.allowed_to?(action) &&
-            (block_given? ? yield(role, self) : true)
-          }
-        else
-          false
         end
+        allowed_to_without_project_access?(action, context, options)
       end
     end
   end
